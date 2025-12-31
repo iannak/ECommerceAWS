9a75ff78292c0b37908da889155282ed0a1bfaf5
annafranz91 <anna.franz@e-deploy.com.br>
2025-12-31 11:41:20 -0300

    preparing function search products

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,25 +1,33 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
-export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult>{
-    const lambdaRequestId = context.awsRequestId;
-    const apiRequestId = event.requestContext.requestId;
-    
-    console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+  const lambdaRequestId = context.awsRequestId;
+  const apiRequestId = event.requestContext.requestId;
 
-    const method = event.httpMethod;
-    
-    if(event.resource === '/products'){
-        if(method === 'GET'){
-        console.log('Fetching products...');
+  console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
 
-        return { 
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Products fetched successfully' })
-        }
+  const method = event.httpMethod;
+
+  if (event.resource === '/products') {
+    if (method === 'GET') {
+      console.log('Fetching products...');
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Products fetched successfully' })
+      };
+    } else if (event.resource === "/products/{id}") {
+      const productId = event.pathParameters!.id as string;
+      console.log(`GET: /products/${productId}`);
+
+      return {
+        statusCode: 200,
+        body: "GET products/{id}"
+      };
     }
-    }
-    return {
-        statusCode: 400,
-        body: JSON.stringify({ message: 'Bad Request' })
-    }
+  }
+  return {
+    statusCode: 400,
+    body: JSON.stringify({ message: 'Bad Request' })
+  };
 }
