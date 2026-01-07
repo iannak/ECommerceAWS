9b5b6ef95186ab2e2f3a2f306a8956a8f29fc9e6
Anna Franz <franz.karols@gmail.com>
2026-01-06 22:46:15 -0300

    feat: createad list products

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -1,7 +1,25 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import { v4 as uuid } from "uuid";
+
 export interface Product {
     id: string;
     productName: string;
     code: string;
     price: number;
     model: string;
+}
+
+export class ProductRepository {
+    private readonly dbClient: DocumentClient;
+    private readonly productsTable: string;
+
+    constructor(dbClient: DocumentClient, productsTable: string) {
+        this.dbClient = dbClient;
+        this.productsTable = productsTable;
+    }
+
+    async getAllProducts(): Promise<Product[]> {
+        const result = await this.dbClient.scan({ TableName: this.productsTable }).promise();
+        return result.Items as Product[];
+    }
 }
