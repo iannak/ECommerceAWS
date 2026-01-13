f74559d849120fd752e6d97d07ee3e366e6c0886
Anna Franz <franz.karols@gmail.com>
2026-01-13 08:28:00 -0300

    feat: function search product

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,4 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { ProductRepository } from "/opt/nodejs/productsLayer";
+import { DynamoDB } from "aws-sdk";
+
+const dynamoDbClient = process.env.PRODUCTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient();
+
+const productRepository = new ProductRepository(ddbClient, dynamoDbClient);
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   const lambdaRequestId = context.awsRequestId;
@@ -12,18 +19,32 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
     if (method === 'GET') {
       console.log('Fetching products...');
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Products fetched successfully' })
-      };
-    } else if (event.resource === "/products/{id}") {
-      const productId = event.pathParameters!.id as string;
-      console.log(`GET: /products/${productId}`);
+      const products = await productRepository.getAllProducts();
 
       return {
         statusCode: 200,
-        body: "GET products/{id}"
+        body: JSON.stringify(products)
       };
+    }
+  } else if (event.resource === "/products/{id}") {
+    if (method === 'GET') {
+      const productId = event.pathParameters!.id as string;
+      console.log(`GET: /products/${productId}`);
+
+      try {
+        const product = await productRepository.getProductById(productId);
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify(product)
+        };
+      } catch (error) {
+        console.error(`Error fetching product with id ${productId}:`, error);
+        return {
+          statusCode: 404,
+          body: JSON.stringify({ message: 'Product not found' })
+        };
+      }
     }
   }
   return {
