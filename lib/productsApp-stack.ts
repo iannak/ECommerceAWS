3f0012204189ef84c584f181a6acc59e3d95c6bf
annafranz91 <anna.franz@e-deploy.com.br>
2026-01-20 22:31:52 -0300

    feat: add module aws-xray-skd-core no bundle function lambda

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,69 +1,86 @@
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as cdk from 'aws-cdk-lib';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
-import * as ssm from "aws-cdk-lib/aws-ssm"
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
-import { Construct } from 'constructs';
+import { Construct } from "constructs";
 
-export class ProductsAppStack extends cdk.Stack { 
-    readonly productsFetchHandler: lambdaNodejs.NodejsFunction;
-    readonly productsAdminHandler: lambdaNodejs.NodejsFunction;
-    readonly productsDdb: dynamodb.Table;
+export class ProductsAppStack extends cdk.Stack {
+  readonly productsFetchHandler: lambdaNodejs.NodejsFunction;
+  readonly productsAdminHandler: lambdaNodejs.NodejsFunction;
+  readonly productsDdb: dynamodb.Table;
 
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-        this.productsDdb = new dynamodb.Table(this, "ProductsDdb", {
-            tableName: "products",
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
-            partitionKey: {
-                name: "id",
-                type: dynamodb.AttributeType.STRING
-            },
-            billingMode: dynamodb.BillingMode.PROVISIONED,
-            readCapacity: 1,
-            writeCapacity: 1
-        })
+    this.productsDdb = new dynamodb.Table(this, "ProductsDdb", {
+      tableName: "products",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
 
-        const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn")
-        const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn)
+    const productsLayerArn = ssm.StringParameter.valueForStringParameter(
+      this,
+      "ProductsLayerVersionArn",
+    );
+    const productsLayer = lambda.LayerVersion.fromLayerVersionArn(
+      this,
+      "ProductsLayerVersionArn",
+      productsLayerArn,
+    );
 
-        this.productsFetchHandler = new lambdaNodejs.NodejsFunction(this, 'ProductsFetchFunction', {
-            functionName: 'ProductsFetchFunction',
-            entry: 'lambda/products/productsFetchFunction.ts',
-            handler: 'handler',
-            memorySize: 512,
-            runtime: lambda.Runtime.NODEJS_18_X,
-            timeout: cdk.Duration.seconds(10),
-            bundling: {
-                minify: true,
-                sourceMap: false,
-            },
-            environment: {
-                PRODUCTS_DDB: this.productsDdb.tableName,
-            },
-            layers: [productsLayer]
-        });
+    this.productsFetchHandler = new lambdaNodejs.NodejsFunction(
+      this,
+      "ProductsFetchFunction",
+      {
+        functionName: "ProductsFetchFunction",
+        entry: "lambda/products/productsFetchFunction.ts",
+        handler: "handler",
+        memorySize: 512,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        timeout: cdk.Duration.seconds(10),
+        bundling: {
+          minify: true,
+          sourceMap: false,
+          nodeModules: ["aws-xray-sdk-core"],
+        },
+        environment: {
+          PRODUCTS_DDB: this.productsDdb.tableName,
+        },
+        layers: [productsLayer],
+      },
+    );
 
-        this.productsDdb.grantReadData(this.productsFetchHandler)
+    this.productsDdb.grantReadData(this.productsFetchHandler);
 
-        this.productsAdminHandler = new lambdaNodejs.NodejsFunction(this, 'ProductsAdminFunction', {
-            functionName: 'ProductsAdminFunction',
-            entry: 'lambda/products/productsAdminFunction.ts',
-            handler: 'handler',
-            memorySize: 512,
-            runtime: lambda.Runtime.NODEJS_18_X,
-            timeout: cdk.Duration.seconds(10),
-            bundling: {
-                minify: true,
-                sourceMap: false,
-            },
-            environment: {
-                PRODUCTS_DDB: this.productsDdb.tableName,
-            },
-            layers: [productsLayer]
-        });
-        this.productsDdb.grantWriteData(this.productsAdminHandler)
-    }
+    this.productsAdminHandler = new lambdaNodejs.NodejsFunction(
+      this,
+      "ProductsAdminFunction",
+      {
+        functionName: "ProductsAdminFunction",
+        entry: "lambda/products/productsAdminFunction.ts",
+        handler: "handler",
+        memorySize: 512,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        timeout: cdk.Duration.seconds(10),
+        bundling: {
+          minify: true,
+          sourceMap: false,
+          nodeModules: ["aws-xray-sdk-core"],
+        },
+        environment: {
+          PRODUCTS_DDB: this.productsDdb.tableName,
+        },
+        layers: [productsLayer],
+      },
+    );
+    this.productsDdb.grantWriteData(this.productsAdminHandler);
+  }
 }
